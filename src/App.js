502f5b15870eab5c842cc1f0c5d081907a07b610
Ryan Woodruff <ryanwoodruff2002@gmail.com>
2022-12-10 20:57:33 -0500

    green

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-      This is the start of something great! 
+      This is the start of something great!
     </div>
   );
 }
